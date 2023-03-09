9b387b057aea9628d35469b0fe0dc5a75c6101b7
Jyotirya Jain <business.jyojain@gmail.com>
2023-03-08 20:10:54 -0800

    readying_data.py file is ready for primary function, README.md is updated to match changes made to this commit (03/08 commit)

--- README.md
+++ README.md
@@ -5,10 +5,16 @@
 
 ### Data Collection
 1. Manually collected raw data from @basket-reference.com and saved into csv `data.csv`
-2. **Features**: Name, Position, Date, Team, Location (Home: 0, At: 1), Opponent, Minutes Played
+2. **Features**: Name, Position, Month, Day, Year, Team, Location (Home: 0, At: 1), Opponent, Minutes Played
 3. **Attributes**: FGA, FGM, 3PA, 3PM, FTA, FTM, Rebounds, Assists, Steal, Block, Turnovers, Points
 
 ### Machine Learning Model Setup
-1. Need to map player names to IDs and team names to IDs, Position to their respective numbers (PG:1, SG:2, SF:3, PF:4, C:5)
-
+1. `readying_data.py` takes `cleaned_data.csv` and performs a variety of operations:
+   1. map *team* to a respective *team_id*
+   2. delete rows where players did not play
+   3. convert *name* to a respective *player_id*
+   4. map *opp* to a respective *team_id*
+   5. convert *position* to their respective number (PG:1, SG:2, SF:3, PF:4, C:5)
+   6. convert *minutes played* to total seconds on field
+   7. convert date to Month, Day, and Year columns
 
--- readying_data.py
+++ readying_data.py
@@ -5,51 +5,122 @@ import pandas as pd
 import random
 
 random.seed(10)
-data = pd.read_csv("cleaned_data.csv")
-
 
 def ready_data():
-    # Convert player names to player IDs
-    player_id = {}
+    data = pd.read_csv("cleaned_data.csv")
+    dnp = ["Inactive", "Did Not Dress", "Did Not Play", "Suspended", "Not With Team", "Player Suspended"]
 
+    player_id = {}
+    team_id = {
+                "ATL":1,
+                "BOS": 2,
+                "BRK": 3,
+                "CHO": 4,
+                "CHI": 5,
+                "CLE": 6,
+                "DAL": 7,
+                "DEN": 8,
+                "DET": 9,
+                "GSW": 10,
+                "HOU": 11,
+                "IND": 12,
+                "LAC": 13,
+                "LAL": 14,
+                "MEM": 15,
+                "MIA": 16,
+                "MIL": 17,
+                "MIN": 18,
+                "NOP": 19,
+                "NYK": 20,
+                "OKC": 21,
+                "ORL": 22,
+                "PHI": 23,
+                "PHO": 24,
+                "POR": 25,
+                "SAC": 26,
+                "SAS": 27,
+                "TOR": 28,
+                "UTA": 29,
+                "WAS": 30
+               }
+    delete = []
     for index, row in data.iterrows():
+        # ignore row where player did not play
+        if str(row['Minutes Played']) in dnp:
+            delete.append(index)
+            continue
+
+        # Convert player names to player IDs
         if row['Name'] == "":
             break
         elif row['Name'] not in player_id:
             player_id.update({row['Name']: random.randint(0, 100000)})
         data.at[index, 'Name'] = player_id[row['Name']]
 
-    # Convert team names to team IDs
-    team_id = {}
-
-    for index, row in data.iterrows():
-        if row['Team'] == "":
-            break
-        elif row['Team'] not in team_id:
-            team_id.update({row['Team']: random.randint(0, 20000)})
+        # Convert team names to team IDs
         data.at[index, 'Team'] = team_id[row['Team']]
 
-    # Convert opponent names to team IDs
-    # opponents are the same as team names, so we need to use the team_id dictionary and map to the opponent names
-    for index, row in data.iterrows():
-        if row['Opp'] == "":
-            break
+        # Convert opponent names to team IDs
+        # opponents are the same as team names, so we need to use the team_id dictionary and map to the opponent names
         data.at[index, 'Opp'] = team_id[row['Opp']]
 
-    # Convert Position to respective number
-    # (PG:1, SG:2, SF:3, PF:4, C:5)
-    position_num = {
-                    'PG': 1,
-                    'SG': 2,
-                    'SF': 3,
-                    'PF': 4,
-                    'C': 5
+        # Convert Position to respective number
+        # (PG:1, SG:2, SF:3, PF:4, C:5)
+        position_num = {
+                        'PG': 1,
+                        'SG': 2,
+                        'SF': 3,
+                        'PF': 4,
+                        'C': 5
                             }
-    for index, row in data.iterrows():
         if row['Position'] == "":
             break
         data.at[index, 'Position'] = position_num[row['Position']]
 
+        # Convert Minutes played to total seconds
+        if row['Minutes Played'] == "":
+            break
+        elif row['Minutes Played'] in dnp:
+            pass
+        else:
+            data.at[index, 'Minutes Played'] = (int(data.at[index, 'Minutes Played'].split(':')[0]) * 60) + (int(data.at[index, 'Minutes Played'].split(':')[1]) * 60)
+
+        # Convert Date to Month, Day, and Year
+        if row['Date'] == "":
+            break
+        temp = data.at[index, 'Date'].split("/")
+        data.at[index, 'Month'] = temp[0]
+        data.at[index, 'Date'] = temp[1]
+        data.at[index, 'Year'] = temp[2]
+
+    # Delete rows where players haven't played
+    data.drop(index=delete)
     # Return finalized data
+    convert_dict = {
+        'Name': int,
+        'Position': int,
+        'Month': int,
+        'Date': int,
+        'Year': int,
+        'Team': int,
+        'Location': int,
+        'Opp': int,
+        'Minutes Played': int,
+        'FGM': float,
+        'FGA': float,
+        '3PM': float,
+        '3PA': float,
+        'FTM': float,
+        'FTA': float,
+        'ORB': float,
+        'DRB': float,
+        'TRB': float,
+        'AST': float,
+        'STL': float,
+        'BLK': float,
+        'TOV': float,
+        'PTS': float
+    }
+    data = data.astype(convert_dict)
     return data
 print(ready_data())
